d8cde5bc70655d9dbbebfcfb695314cce87dea00
Soheil <Soheil@DESKTOP-UCNTFOP>
2020-02-21 14:50:09 +0330

    configuration added

--- BuildScript/Build.cs
+++ BuildScript/Build.cs
@@ -116,7 +116,8 @@ class Build : NukeBuild
             Logger.Log(LogLevel.Warning,$"temp path : {TemporaryDirectory}");
             DotNetPack(a =>
                 a.SetProject(Solution)
-                    .SetOutputDirectory(TemporaryDirectory)
+                    .SetConfiguration(Configuration)
+                    .SetOutputDirectory(ArtifactsPath)
                     .SetAuthors("Soheil Karami")
                     .SetNoRestore(true)
                     .SetNoBuild(true));
