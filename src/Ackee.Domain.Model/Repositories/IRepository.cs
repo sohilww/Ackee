537a247642240544da6123bcb32ce4be52b68eff
Soheil <Soheil@DESKTOP-UCNTFOP>
2020-03-15 10:48:42 +0330

    repository added to project

---
+++ src/Ackee.Domain.Model/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Ackee.Domain.Model.Repositories
+{
+    public interface IRepository
+    {
+        
+    }
+
+    public interface IRepository<T, TKey> : IRepository where T : IAggregateRoot
+    {
+        Task<TKey> GetNextId();
+        Task Create(T aggregate);
+        Task Remove(T aggregate);
+        Task<T> Get(TKey key);
+        Task<T> Find(Expression<Func<T, bool>> predicate);
+        Task<List<T>> FindAll(Expression<Func<T, bool>> predicate);
+    }
+}
