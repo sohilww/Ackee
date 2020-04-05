6a1175da3c1db630770ab0172d07876a5cb623ac
Soheil <Soheil@DESKTOP-UCNTFOP>
2020-04-05 23:56:50 +0430

    cqrs model convention and version convetion added

---
+++ src/Ackee.AspNetCore/CQRSModelConvention.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System;
+
+namespace Ackee.AspNetCore
+{
+    public class CQRSModelConvention : IControllerModelConvention
+    {
+        public void Apply(ControllerModel controller)
+        {
+            var controllerName = controller.ControllerName.Replace("Query", "", StringComparison.OrdinalIgnoreCase);
+            foreach (var selector in controller.Selectors)
+            {
+                if (!(selector.AttributeRouteModel is null))
+                    selector.AttributeRouteModel.Template = $"api/{controllerName}";
+            }
+
+        }
+    }
+}
---
+++ src/Ackee.AspNetCore/VersionModelConvention.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace Ackee.AspNetCore
+{
+    public class VersionModelConvention : IControllerModelConvention
+    {
+        private readonly string _version;
+        public VersionModelConvention(string version = "V1")
+        {
+            _version = version;
+        }
+        public void Apply(ControllerModel controller)
+        {
+            var controllerName = controller.ControllerName;
+            foreach (var selector in controller.Selectors)
+            {
+                if (!(selector.AttributeRouteModel is null))
+                    selector.AttributeRouteModel.Template = $"{_version}/api/{controllerName}";
+            }
+
+        }
+    }
+}
