13acbda1a58f4619b96b107a80ead598fae3e8ca
Soheil Karami <sohil_ww@ymail.com>
2020-06-18 23:14:50 +0430

    Specification Pattern added

---
+++ src/Ackee.Specification/LinqSpecification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Ackee.Specification
+{
+    public abstract class LinqSpecification<T> : CompositeSpecification<T>
+    {
+        public abstract Expression<Func<T, bool>> AsExpression();
+        public override bool IsSatisfiedBy(T candidate) => AsExpression().Compile()(candidate);
+    }
+}
---
+++ src/Ackee.Specification/OrSpecification.cs
@@ -0,0 +1,16 @@
+﻿namespace Ackee.Specification
+{
+    public class OrSpecification<T> : CompositeSpecification<T>
+    {
+        ISpecification<T> left;
+        ISpecification<T> right;
+
+        public OrSpecification(ISpecification<T> left, ISpecification<T> right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+
+        public override bool IsSatisfiedBy(T candidate) => left.IsSatisfiedBy(candidate) || right.IsSatisfiedBy(candidate);
+    }
+}
