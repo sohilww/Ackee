891279dcf34489b636ca4c6e6f2fa9bdc3a36451
pakan.r <pakan.rahmani@gmail.com>
2020-09-02 12:17:13 +0430

    EFCommandHandler Decorator Added

---
+++ src/Ackee.DataAccess.EfCore/EFCommandHandlerDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Ackee.Application;
+
+namespace Ackee.DataAccess.EfCore
+{
+    public class EfCommandHandlerDecorator<T> : ICommandHandler<T>
+    {
+        private readonly ICommandHandler<T> _commandHandler;
+        private readonly AckeeDbContext _ackeeDbContext;
+
+        public EfCommandHandlerDecorator(ICommandHandler<T> commandHandler, AckeeDbContext ackeeDbContext)
+        {
+            _commandHandler = commandHandler;
+            _ackeeDbContext = ackeeDbContext;
+        }
+        public async Task Handle(T command)
+        {
+            await _commandHandler.Handle(command);
+            await _ackeeDbContext.SaveChangesAsync();
+        }
+    }
+}
