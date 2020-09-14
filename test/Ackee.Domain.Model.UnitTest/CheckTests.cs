ae91886c8c1e52c101440dd1f02e6b842f298cc1
soheil karami <soheilkarami@outlook.com>
2020-09-14 18:41:23 +0430

    null or empty test added

--- test/Ackee.Domain.Model.UnitTest/CheckTests.cs
+++ test/Ackee.Domain.Model.UnitTest/CheckTests.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using Ackee.Core;
+﻿using Ackee.Core;
 using Ackee.Core.Exceptions;
-using Ackee.Domain.Model.TestUtility;
 using Ackee.TestHelpers;
 using FluentAssertions;
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace Ackee.Domain.Model.UnitTest
 {
     public class CheckTests
     {
-        private object _nullObject=null;
+        private object _nullObject = null;
 
         [Fact]
         public void When_object_is_null_it_throws_exception()
@@ -31,7 +31,7 @@ namespace Ackee.Domain.Model.UnitTest
         [Fact]
         public void When_string_is_empty_it_throws_exception()
         {
-            Action action = () => Check.NotNullOrWhiteSpace(string.Empty) ;
+            Action action = () => Check.NotNullOrWhiteSpace(string.Empty);
 
             action.Should().Throw<ArgumentNullAckeeException>();
         }
@@ -39,9 +39,28 @@ namespace Ackee.Domain.Model.UnitTest
         public void When_string_is_empty_it_throws_exception_with_parameters_name()
         {
             var parameterName = "name";
-            Action action = () => Check.NotNullOrWhiteSpace(string.Empty,parameterName);
+            Action action = () => Check.NotNullOrWhiteSpace(string.Empty, parameterName);
 
             action.Should().Throw<ArgumentNullAckeeException>().ForField(parameterName);
+        }
+
+        [Fact]
+        public void when_collection_is_null_throw_exception()
+        {
+            List<int> nullList = null;
+
+            Action action = () => Check.NotNullOrEmpty(nullList, "");
+
+            action.Should().Throw<ArgumentNullAckeeException>();
+        }
+        [Fact]
+        public void when_collection_is_empty_throw_exception()
+        {
+            List<int> nullList = new List<int>();
+
+            Action action = () => Check.NotNullOrEmpty(nullList, "");
+
+            action.Should().Throw<ArgumentNullAckeeException>();
         }
     }
 }
